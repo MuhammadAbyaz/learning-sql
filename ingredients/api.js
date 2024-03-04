bf809ff4d7a97d28d97c0992c0cb6b71e73126bd
Abyaz <58655276+MuhammadAbyaz@users.noreply.github.com>
2024-03-04 18:44:18 +0500

    solve both recipes and ingredients question

--- ingredients/api.js
+++ ingredients/api.js
@@ -13,14 +13,26 @@ router.get("/client.js", (_, res) =>
  */
 
 // connect to postgres
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recipeguru",
+  password: "postgres",
+  port: 5432,
+});
 
 router.get("/type", async (req, res) => {
   const { type } = req.query;
   console.log("get ingredients", type);
 
+  const { rows } = await pool.query(
+    "SELECT * FROM ingredients WHERE type=$1 ",
+    [type]
+  );
   // return all ingredients of a type
 
-  res.status(501).json({ status: "not implemented", rows: [] });
+  res.json({ rows });
 });
 
 router.get("/search", async (req, res) => {
@@ -28,10 +40,20 @@ router.get("/search", async (req, res) => {
   page = page ? page : 0;
   console.log("search ingredients", term, page);
 
+  let whereClause = "";
+  const params = [page * 5];
+  if (term) {
+    whereClause = "WHERE CONCAT(title,type) ILIKE $2";
+    params.push(`%${term}%`);
+  }
+  const { rows } = await pool.query(
+    `SELECT *, COUNT(*) OVER():: INT AS total_count FROM ingredients ${whereClause} OFFSET $1 LIMIT 5`,
+    params
+  );
   // return all columns as well as the count of all rows as total_count
   // make sure to account for pagination and only return 5 rows at a time
 
-  res.status(501).json({ status: "not implemented", rows: [] });
+  res.json({ rows });
 });
 
 /**
--- recipes/api.js
+++ recipes/api.js
@@ -22,15 +22,27 @@ router.get("/detail", (_, res) =>
  */
 
 // connect to postgres
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recipeguru",
+  password: "lol",
+  port: 5432,
+});
 
 router.get("/search", async function (req, res) {
   console.log("search recipes");
-
+  const { rows } = await pool.query(`SELECT DISTINCT ON (r.recipe_id)
+recipeguru-# r.recipe_id, r.title, COALESCE(rp.url,'default.jpg') AS url
+recipeguru-# FROM recipes r
+recipeguru-# LEFT JOIN recipes_photos rp
+recipeguru-# ON r.recipe_id = rp.recipe_id;`);
   // return recipe_id, title, and the first photo as url
   //
   // for recipes without photos, return url as default.jpg
 
-  res.status(501).json({ status: "not implemented", rows: [] });
+  res.json({ rows });
 });
 
 router.get("/get", async (req, res) => {
@@ -42,16 +54,44 @@ router.get("/get", async (req, res) => {
   //    name the ingredient type `ingredient_type`
   //    name the ingredient title `ingredient_title`
   //
+
+  const ingredientsPromise = pool.query(
+    `
+  SELECT i.image AS "ingredient_image", i.type AS "ingredient_type", i.title AS "ingredient_title" FROM ingredients recipe_ingredients ri JOIN ingredients i ON i.id = ri.ingredient_id 
+  WHERE ri.recipe_id = $1;
+  `,
+    [recipeId]
+  );
   //
   // return all photo rows as photos
   //    return the title, body, and url (named the same)
   //
+  const photosPromise = pool.query(
+    `SELECT r.title, r.body, COALESCE(rp.url, "default.jpg")
+    AS url FROM recipes r
+    LEFT JOIN 
+    recipes_photos rp
+    ON rp.recipe_id = r.recipe_id
+    WHERE r.recipe_id = $1
+    `,
+    [recipeId]
+  );
+
+  const [{ rows: photoRows }, { rows: ingredientRows }] = await Promise.all([
+    photosPromise,
+    ingredientsPromise,
+  ]);
+
+  res.json({
+    ingredient: ingredientRows,
+    photos: photoRows.map((photo) => photo.url),
+    title: photoRows[0].title,
+    body: photoRows[0].body,
+  });
   //
   // return the title as title
   // return the body as body
   // if no row[0] has no photo, return it as default.jpg
-
-  res.status(501).json({ status: "not implemented" });
 });
 /**
  * Student code ends here
